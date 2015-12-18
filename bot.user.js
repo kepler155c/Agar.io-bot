00169e5af4e8cf4441da82ece034b77d9272bf3f
Joe O'Donnell <joeod@contiigo.com>
2015-12-18 18:23:25 -0500

    autosplit

--- bot.user.js
+++ bot.user.js
@@ -24,12 +24,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.651
+// @version     3.652
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.651;
+var aposBotVersion = 3.652;
 
 //TODO: Team mode
 //      Detect when people are merging
@@ -931,10 +931,12 @@ function AposBot() {
 
                         var enemyDistance = this.computeDistance(allPossibleThreats[i].x, allPossibleThreats[i].y, player[k].x, player[k].y, allPossibleThreats[i].size);
                         allPossibleThreats[i].enemyDist = enemyDistance;
+                    	drawCircle(allPossibleThreats[i].x, allPossibleTargets[i].y, allPossibleTargets[i].size + 30, 5);
 
                     }
 
-                    if (allPossibleTargets.length == 0) {
+                    if (allPossibleThreats.length == 0)
+                        var safeToSplit = false;
                         for (var i = 0; i < allPossibleTargets.length; i++) {
 
                             var enemyDistance = this.computeDistance(allPossibleTargets[i].x, allPossibleTargets[i].y, player[k].x, player[k].y, allPossibleTargets[i].size);
