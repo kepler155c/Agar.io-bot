e37ebbb7cad01ecaeaff6200f0a1b56a995cd96b
Joe O'Donnell <joeod@contiigo.com>
2015-12-18 20:24:43 -0500

    autosplit

--- bot.user.js
+++ bot.user.js
@@ -24,12 +24,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.666
+// @version     3.667
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.666;
+var aposBotVersion = 3.667;
 
 //TODO: Team mode
 //      Detect when people are merging
@@ -951,7 +951,7 @@ function AposBot() {
                     	drawCircle(allPossibleThreats[i].x, allPossibleThreats[i].y, allPossibleThreats[i].size + 30, 5);
 
                     }
-console.log(player.length + ' ' + allPossibleThreats.length + ' ' + isSplitting);
+//console.log(player.length + ' ' + allPossibleThreats.length + ' ' + isSplitting);
                     if (allPossibleThreats.length == 0 && !isSplitting && player.length == 1) {
 
                     	var allPossibleTargets = allIsAll[3];
