f834c79756a9b02deee6fcb921a4f5bb9c88d48f
Joe O'Donnell <joeod@contiigo.com>
2015-12-20 10:26:01 -0500

    ...

--- bot.user.js
+++ bot.user.js
@@ -24,12 +24,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.707
+// @version     3.708
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.707;
+var aposBotVersion = 3.708;
 
 //TODO: Team mode
 //      Detect when people are merging
