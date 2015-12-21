18fba317227e5669bed8739daf1c2c8be94b63f3
Joe O'Donnell <joeod@contiigo.com>
2015-12-21 04:45:30 -0500

    food distance

--- bot.user.js
+++ bot.user.js
@@ -24,12 +24,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.761
+// @version     3.762
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.761;
+var aposBotVersion = 3.762;
 
 //TODO: Team mode
 //      Detect when people are merging
@@ -1226,6 +1226,7 @@ function AposBot() {
             	var closestInfo = this.closestCell(player, cluster.x, cluster.y);
                 cluster.clusterSize = closestInfo.distance / (cluster.size * multiplier); // - closestInfo.distance;
                 cluster.closestCell = closestInfo.cell;
+                console.log(closetInfo);
             }
             
             var bestFoodI = 0;
