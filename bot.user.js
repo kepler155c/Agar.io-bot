c33b28f5a6ed296ce2de8030d80ae2be9653539e
Joe O'Donnell <joeod@contiigo.com>
2015-12-21 21:28:19 -0500

    mass

--- bot.user.js
+++ bot.user.js
@@ -24,12 +24,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.819
+// @version     3.820
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.819;
+var aposBotVersion = 3.820;
 
 //TODO: Team mode
 //      Detect when people are merging
@@ -1001,7 +1001,7 @@ function AposBot() {
             
             var enemyCanSplit = this.isSplitTarget(threat, player.smallestCell);
             
-            if (enemyCanSplit && this.getRatio(threat, player.smallestCell) > 12) {
+            if (enemyCanSplit && this.getRatio(threat, player.smallestCell) > 20) {
             	enemyCanSplit = false;
             }
 
