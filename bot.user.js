2a05ee07a99af12fda6dcc511d94d814eb8483f4
Joe O'Donnell <joeod@contiigo.com>
2015-12-24 12:01:42 -0500

    refactor

--- bot.user.js
+++ bot.user.js
@@ -33,12 +33,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.923
+// @version     3.924
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.923;
+var aposBotVersion = 3.924;
 
 var constants = {
 	safeDistance: 150,
@@ -74,6 +74,166 @@ Number.prototype.mod = function(n) {
     return ((this % n) + n) % n;
 };
 */
+
+var Player = function() {
+	this.cells = [];
+	this.isAlive = true;
+	this.isReviving = false;
+	this.isSplitting = false;
+	this.isLuring = false;
+
+    this.food = [];
+    this.threats = [];
+    this.viruses = [];
+    this.splitTargets = [];
+    this.enemies = [];
+    
+    this.chasing = 0;
+    this.splitVelocity = 0;
+    this.splitLocation = null;
+    this.fuseTimer = null;
+};
+
+Player.prototype.setCells = function(cells) {
+	this.cells = cells;
+	this.isAlive = this.cells.length > 0;
+	this.mass = 0;
+    this.smallestCell = cells[0];
+    this.largestCell = cells[0];
+    
+	for (var i = 0; i < cells.length; i++) {
+		var cell = cells[i];
+		
+	    cell.mass = cell.size * cell.size / 100;
+
+	    this.mass = this.mass + cell.mass;
+
+    	if (cell.size < this.smallestCell.size) {
+    		this.smallestCell = cell;
+    	}
+    	if (cell.size > this.largestCell.size) {
+    		this.largestCell = cell;
+    	}
+    	
+    	if (cells.length > 0) {
+    		if (!cell.fuseTimer) {
+    			cell.fuseTimer = Date.now() + (30 + cell.mass * 0.02) * 1000;
+    		}
+    	} else {
+    		cell.fuseTimer = null;
+    	}
+	}
+
+	if (cells.length > 0) {
+		
+		var enclosingCell = cells[0];
+		
+		if (cells.length > 1) {
+			enclosingCell = enclosingCircle(cells);
+		}
+		this.size = enclosingCell.size;
+		this.x = enclosingCell.x;
+		this.y = enclosingCell.y;
+	}
+};
+
+function randomizedList(array) {
+  var i,
+      n = (array = array.slice()).length,
+      head = null,
+      node = head;
+  while (n) {
+    var next = {id: array.length - n, value: array[n - 1], next: null};
+    if (node) node = node.next = next;
+    else node = head = next;
+    array[i] = array[--n];
+  }
+  return {head: head, tail: node};
+}
+// Returns the smallest circle that contains the specified circles.
+function enclosingCircle(circles) {
+  return enclosingCircleIntersectingCircles(randomizedList(circles), []);
+}
+// Returns the smallest circle that contains the circles L
+// and intersects the circles B.
+function enclosingCircleIntersectingCircles(L, B) {
+  var circle,
+      l0 = null,
+      l1 = L.head,
+      l2,
+      p1;
+  switch (B.length) {
+    case 1: circle = B[0]; break;
+    case 2: circle = circleIntersectingTwoCircles(B[0], B[1]); break;
+    case 3: circle = circleIntersectingThreeCircles(B[0], B[1], B[2]); break;
+  }
+  while (l1) {
+    p1 = l1.value, l2 = l1.next;
+    if (!circle || !circleContainsCircle(circle, p1)) {
+      // Temporarily truncate L before l1.
+      if (l0) L.tail = l0, l0.next = null;
+      else L.head = L.tail = null;
+      B.push(p1);
+      circle = enclosingCircleIntersectingCircles(L, B); // Note: reorders L!
+      B.pop();
+      // Move l1 to the front of L and reconnect the truncated list L.
+      if (L.head) l1.next = L.head, L.head = l1;
+      else l1.next = null, L.head = L.tail = l1;
+      l0 = L.tail, l0.next = l2;
+    } else {
+      l0 = l1;
+    }
+    l1 = l2;
+  }
+  L.tail = l0;
+  return circle;
+}
+// Returns true if the specified circle1 contains the specified circle2.
+function circleContainsCircle(circle1, circle2) {
+  var xc0 = circle1.x - circle2.x,
+      yc0 = circle1.y - circle2.y;
+  return Math.sqrt(xc0 * xc0 + yc0 * yc0) < circle1.size - circle2.size + 1e-6;
+}
+// Returns the smallest circle that intersects the two specified circles.
+function circleIntersectingTwoCircles(circle1, circle2) {
+  var x1 = circle1.x, y1 = circle1.y, r1 = circle1.size,
+      x2 = circle2.x, y2 = circle2.y, r2 = circle2.size,
+      x12 = x2 - x1, y12 = y2 - y1, r12 = r2 - r1,
+      l = Math.sqrt(x12 * x12 + y12 * y12);
+  return {
+    x: (x1 + x2 + x12 / l * r12) / 2,
+    y: (y1 + y2 + y12 / l * r12) / 2,
+    size: (l + r1 + r2) / 2
+  };
+}
+// Returns the smallest circle that intersects the three specified circles.
+function circleIntersectingThreeCircles(circle1, circle2, circle3) {
+  var x1 = circle1.x, y1 = circle1.y, r1 = circle1.size,
+      x2 = circle2.x, y2 = circle2.y, r2 = circle2.size,
+      x3 = circle3.x, y3 = circle3.y, r3 = circle3.size,
+      a2 = 2 * (x1 - x2),
+      b2 = 2 * (y1 - y2),
+      c2 = 2 * (r2 - r1),
+      d2 = x1 * x1 + y1 * y1 - r1 * r1 - x2 * x2 - y2 * y2 + r2 * r2,
+      a3 = 2 * (x1 - x3),
+      b3 = 2 * (y1 - y3),
+      c3 = 2 * (r3 - r1),
+      d3 = x1 * x1 + y1 * y1 - r1 * r1 - x3 * x3 - y3 * y3 + r3 * r3,
+      ab = a3 * b2 - a2 * b3,
+      xa = (b2 * d3 - b3 * d2) / ab - x1,
+      xb = (b3 * c2 - b2 * c3) / ab,
+      ya = (a3 * d2 - a2 * d3) / ab - y1,
+      yb = (a2 * c3 - a3 * c2) / ab,
+      A = xb * xb + yb * yb - 1,
+      B = 2 * (xa * xb + ya * yb + r1),
+      C = xa * xa + ya * ya - r1 * r1,
+      r = (-B - Math.sqrt(B * B - 4 * A * C)) / (2 * A);
+  return {
+    x: xa + xb * r + x1,
+    y: ya + yb * r + y1,
+    size: r
+  };
+}
 
 Array.prototype.peek = function() {
     return this[this.length - 1];
@@ -152,6 +312,8 @@ function AposBot() {
             this.toggleFollow = !this.toggleFollow;
         }
     };
+    
+    this.player = new Player();
 
     this.displayText = function() {
         return ["Q - Follow Mouse: " + (this.toggleFollow ? "On" : "Off")];
@@ -746,6 +908,7 @@ function AposBot() {
         //drawPoint(blob2.x, blob2.y, 3, "" + blob1Range);
     };
 
+    /*
     this.debugAngle = function(angle, text) {
         var player = getPlayer();
         var cell = player.cells[0];
@@ -753,6 +916,7 @@ function AposBot() {
         drawLine(cell.x, cell.y, line1[0], line1[1], 5);
         drawPoint(line1[0], line1[1], 5, "" + text);
     };
+    */
 
     //TODO: Don't let this function do the radius math.
     this.getEdgeLinesFromPoint = function(blob1, blob2, radius) {
@@ -1497,10 +1661,12 @@ function AposBot() {
      * This is the main bot logic. This is called quite often.
      * @return A 2 dimensional array with coordinates for every cells.  [[x, y], [x, y]]
      */
-    this.mainLoop = function() {
-        var player = getPlayer();
+    this.mainLoop = function(cells) {
+        var player = this.player;
         var interNodes = getMemoryCells();
         var i;
+        
+        player.setCells(cells);
 
         if ( /*!toggle*/ 1) {
             //The following code converts the mouse position into an
--- launcher.user.js
+++ launcher.user.js
@@ -20,14 +20,13 @@ SOFTWARE.*/
 // @name        AposLauncher
 // @namespace   AposLauncher
 // @include     http://agar.io/*
-// @version     4.219
+// @version     4.220
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposLauncherVersion = 4.219;
+var aposLauncherVersion = 4.220;
 
 var showAd = true;
-var badSize = 1500;
 
 Number.prototype.mod = function(n) {
     return ((this % n) + n) % n;
@@ -38,168 +37,6 @@ Array.prototype.peek = function() {
 };
 
 var sha = "efde0488cc2cc176db48dd23b28a20b90314352b";
-
-var Player = function() {
-	this.cells = [];
-	this.isAlive = true;
-	this.isReviving = false;
-	this.isSplitting = false;
-	this.isLuring = false;
-
-    this.food = [];
-    this.threats = [];
-    this.viruses = [];
-    this.splitTargets = [];
-    this.enemies = [];
-    
-    this.chasing = 0;
-    this.splitVelocity = 0;
-    this.splitLocation = null;
-    this.fuseTimer = null;
-};
-
-Player.prototype.setCells = function(cells) {
-	this.cells = cells;
-	this.isAlive = this.cells.length > 0;
-	this.mass = 0;
-    this.smallestCell = cells[0];
-    this.largestCell = cells[0];
-    
-	for (var i = 0; i < cells.length; i++) {
-		var cell = cells[i];
-		
-	    cell.mass = cell.size * cell.size / 100;
-
-	    this.mass = this.mass + cell.mass;
-
-    	if (cell.size < this.smallestCell.size) {
-    		this.smallestCell = cell;
-    	}
-    	if (cell.size > this.largestCell.size) {
-    		this.largestCell = cell;
-    	}
-    	
-    	if (cells.length > 0) {
-    		if (!cell.fuseTimer) {
-    			cell.fuseTimer = Date.now() + (30 + cell.mass * 0.02) * 1000;
-    		}
-    	} else {
-    		cell.fuseTimer = null;
-    	}
-	}
-
-	if (cells.length > 0) {
-		
-		var enclosingCell = cells[0];
-		
-		if (cells.length > 1) {
-			enclosingCell = enclosingCircle(cells);
-		}
-		this.size = enclosingCell.size;
-		this.x = enclosingCell.x;
-		this.y = enclosingCell.y;
-	}
-}
-
-function randomizedList(array) {
-  var i,
-      n = (array = array.slice()).length,
-      head = null,
-      node = head;
-  while (n) {
-    var next = {id: array.length - n, value: array[n - 1], next: null};
-    if (node) node = node.next = next;
-    else node = head = next;
-    array[i] = array[--n];
-  }
-  return {head: head, tail: node};
-}
-// Returns the smallest circle that contains the specified circles.
-function enclosingCircle(circles) {
-  return enclosingCircleIntersectingCircles(randomizedList(circles), []);
-}
-// Returns the smallest circle that contains the circles L
-// and intersects the circles B.
-function enclosingCircleIntersectingCircles(L, B) {
-  var circle,
-      l0 = null,
-      l1 = L.head,
-      l2,
-      p1;
-  switch (B.length) {
-    case 1: circle = B[0]; break;
-    case 2: circle = circleIntersectingTwoCircles(B[0], B[1]); break;
-    case 3: circle = circleIntersectingThreeCircles(B[0], B[1], B[2]); break;
-  }
-  while (l1) {
-    p1 = l1.value, l2 = l1.next;
-    if (!circle || !circleContainsCircle(circle, p1)) {
-      // Temporarily truncate L before l1.
-      if (l0) L.tail = l0, l0.next = null;
-      else L.head = L.tail = null;
-      B.push(p1);
-      circle = enclosingCircleIntersectingCircles(L, B); // Note: reorders L!
-      B.pop();
-      // Move l1 to the front of L and reconnect the truncated list L.
-      if (L.head) l1.next = L.head, L.head = l1;
-      else l1.next = null, L.head = L.tail = l1;
-      l0 = L.tail, l0.next = l2;
-    } else {
-      l0 = l1;
-    }
-    l1 = l2;
-  }
-  L.tail = l0;
-  return circle;
-}
-// Returns true if the specified circle1 contains the specified circle2.
-function circleContainsCircle(circle1, circle2) {
-  var xc0 = circle1.x - circle2.x,
-      yc0 = circle1.y - circle2.y;
-  return Math.sqrt(xc0 * xc0 + yc0 * yc0) < circle1.size - circle2.size + 1e-6;
-}
-// Returns the smallest circle that intersects the two specified circles.
-function circleIntersectingTwoCircles(circle1, circle2) {
-  var x1 = circle1.x, y1 = circle1.y, r1 = circle1.size,
-      x2 = circle2.x, y2 = circle2.y, r2 = circle2.size,
-      x12 = x2 - x1, y12 = y2 - y1, r12 = r2 - r1,
-      l = Math.sqrt(x12 * x12 + y12 * y12);
-  return {
-    x: (x1 + x2 + x12 / l * r12) / 2,
-    y: (y1 + y2 + y12 / l * r12) / 2,
-    size: (l + r1 + r2) / 2
-  };
-}
-// Returns the smallest circle that intersects the three specified circles.
-function circleIntersectingThreeCircles(circle1, circle2, circle3) {
-  var x1 = circle1.x, y1 = circle1.y, r1 = circle1.size,
-      x2 = circle2.x, y2 = circle2.y, r2 = circle2.size,
-      x3 = circle3.x, y3 = circle3.y, r3 = circle3.size,
-      a2 = 2 * (x1 - x2),
-      b2 = 2 * (y1 - y2),
-      c2 = 2 * (r2 - r1),
-      d2 = x1 * x1 + y1 * y1 - r1 * r1 - x2 * x2 - y2 * y2 + r2 * r2,
-      a3 = 2 * (x1 - x3),
-      b3 = 2 * (y1 - y3),
-      c3 = 2 * (r3 - r1),
-      d3 = x1 * x1 + y1 * y1 - r1 * r1 - x3 * x3 - y3 * y3 + r3 * r3,
-      ab = a3 * b2 - a2 * b3,
-      xa = (b2 * d3 - b3 * d2) / ab - x1,
-      xb = (b3 * c2 - b2 * c3) / ab,
-      ya = (a3 * d2 - a2 * d3) / ab - y1,
-      yb = (a2 * c3 - a3 * c2) / ab,
-      A = xb * xb + yb * yb - 1,
-      B = 2 * (xa * xb + ya * yb + r1),
-      C = xa * xa + ya * ya - r1 * r1,
-      r = (-B - Math.sqrt(B * B - 4 * A * C)) / (2 * A);
-  return {
-    x: xa + xb * r + x1,
-    y: ya + yb * r + y1,
-    size: r
-  };
-}
-
-var playerInstance = new Player();
 
 function getLatestCommit() {
     window.jQuery.ajax({
@@ -276,7 +113,7 @@ console.log("Running Bot Launcher!");
         if (82 == e.keyCode) {
             console.log("ToggleDraw");
             toggleDraw = !toggleDraw;
-            interNodes = [];
+//            interNodes = [];
         }
         if (68 == e.keyCode) {
             window.setDarkTheme(!getDarkBool());
@@ -591,7 +428,6 @@ console.log("Running Bot Launcher!");
             return b
         }
         var c = 0;
-        
         240 == a.getUint8(c) && (c += 5);
         switch (a.getUint8(c++)) {
             case 16:
@@ -912,17 +748,16 @@ console.log("Running Bot Launcher!");
             Sa(false);
         }
         
-        var player = getPlayer();
-        
-        if (!player.isAlive && !player.isReviving && ~~(getCurrentScore() / 100) > 0) {
-            console.log("Dead: " + ~~(getCurrentScore() / 100));        }
+        if (getPlayer().length == 0 && !reviving && ~~(getCurrentScore() / 100) > 0) {
+            console.log("Dead: " + ~~(getCurrentScore() / 100));
+		}
 
-        if (!player.isAlive && !firstStart) {
+        if (getPlayer().length == 0 && !firstStart) {
             console.log("Revive");
             setNick(originalName);
-            player.isReviving = true;
-        } else if (player.isAlive && player.isReviving) {
-            player.isReviving = false;
+            reviving = true;
+        } else if (getPlayer().length > 0 && reviving) {
+            reviving = false;
             console.log("Done Reviving!");
         }
 
@@ -1017,6 +852,7 @@ console.log("Running Bot Launcher!");
     	var splitdown = $.Event("keydown", { keyCode: 32}); //space button
         var splitup = $.Event("keyup", { keyCode: 32}); //space button
 
+
         var a, b = Date.now();
         ++zb;
         C = b;
@@ -1063,15 +899,15 @@ console.log("Running Bot Launcher!");
         for (d = 0; d < v.length; d++) v[d].w(f);
         for (d = 0; d < Q.length; d++) Q[d].w(f);
         //UPDATE
-        if (getPlayer().isAlive) {
-            var moveLoc = window.botList[botIndex].mainLoop();
+        if (getPlayer().length > 0) {
+            var moveLoc = window.botList[botIndex].mainLoop(k);
             if (!toggle) {
                 setPoint(moveLoc[0], moveLoc[1]);
                 if (moveLoc[2]) {
-                    //$("body").trigger(splitdown);
-                    //$("body").trigger(splitup);
-                	//console.log('split attempt');
-					window.opCode(17);
+                    $("body").trigger(splitdown);
+                    $("body").trigger(splitup);
+                	console.log('split attempt');
+					//window.opCode(17);
                 }
                 if (moveLoc[3]) {
                     window.opCode(21);
@@ -1102,7 +938,7 @@ console.log("Running Bot Launcher!");
         var currentDate = new Date();
 
         var nbSeconds = 0;
-        if (getPlayer().isAlive) {
+        if (getPlayer().length > 0) {
             //nbSeconds = currentDate.getSeconds() + currentDate.getMinutes() * 60 + currentDate.getHours() * 3600 - lifeTimer.getSeconds() - lifeTimer.getMinutes() * 60 - lifeTimer.getHours() * 3600;
             nbSeconds = (currentDate.getTime() - lifeTimer.getTime()) / 1000;
         }
@@ -1270,9 +1106,6 @@ console.log("Running Bot Launcher!");
     }
 
     function drawStats(d) {
-    	
-    	var player = getPlayer();
-    	
         d.save();
 
         sessionScore = Math.max(getCurrentScore(), sessionScore);
@@ -1654,6 +1487,7 @@ console.log("Running Bot Launcher!");
                 lifeTimer = new Date(),
                 bestTime = 0,
                 botIndex = 0,
+                reviving = false,
                 message = [],
 
                 q = null,
@@ -1733,7 +1567,7 @@ console.log("Running Bot Launcher!");
                     //UPDATE
                     firstStart = false;
                     originalName = a;
-                    if (!getPlayer().isAlive) {
+                    if (getPlayer().length == 0) {
                         lifeTimer = new Date();
                     }
 
@@ -2083,10 +1917,7 @@ console.log("Running Bot Launcher!");
                  * @return Player's cells
                  */
                 window.getPlayer = function() {
-                	
-                	playerInstance.setCells(k);
-                	
-                    return playerInstance;
+                    return k;
                 }
 
                 /**
@@ -2502,9 +2333,6 @@ console.log("Running Bot Launcher!");
                     },
                     w: function(a) {
                         if (this.N()) {
-if (this.size > badSize) {
-    //console.log('skipping - size is ' + this.size)
-} else {
                             ++this.Y;
                             var b = 0 < this.id && !this.h && !this.n && .4 > h;
                             5 > this.I() && (b = !0);
@@ -2533,7 +2361,6 @@ if (this.size > badSize) {
                             }
                             a.closePath();
                             d = this.name.toLowerCase();
-
                             !this.n && kb && ":teams" != P ? -1 != ob.indexOf(d) ? (U.hasOwnProperty(d) || (U[d] = new Image, (d == "notreallyabot" ? U[d].src = "http://i.imgur.com/q5FdCkx.png" : U[d].src = "skins/" +
                                 d + ".png")), c = 0 != U[d].width && U[d].complete ? U[d] : null) : c = null : c = null;
                             c = (e = c) ? -1 != Hb.indexOf(d) : !1;
@@ -2562,7 +2389,6 @@ if (this.size > badSize) {
                                 b - ~~(g / 2), p, g));
                             a.restore()
                         }
-        }
                     }
                 };
                 va.prototype = {
