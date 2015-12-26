12c5e32fa1ef8fb14ebc3e67c5971ea25c665267
Joe O'Donnell <joeod@contiigo.com>
2015-12-26 02:03:27 -0500

    intercept

--- bot.user.js
+++ bot.user.js
@@ -35,12 +35,12 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1008
+// @version     3.1009
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
 
-var aposBotVersion = 3.1008;
+var aposBotVersion = 3.1009;
 
 var constants = {
 	safeDistance: 150,
@@ -787,7 +787,7 @@ function AposBot() {
 
         	if (cluster.cell) {
         		
-	    		var intercept = this.predictPosition(cluster.cell, 600);
+	    		var intercept = this.predictPosition(cluster.cell, 80);
 	
 	    		if (intercept.length > 0) {
 	    			
