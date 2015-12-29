91be42fb6eaac7fec56d4c129d986a3af75b641c
Joe O'Donnell <joeod@contiigo.com>
2015-12-29 03:29:42 -0500

    ...

--- launcher.user.js
+++ launcher.user.js
@@ -20,11 +20,11 @@ SOFTWARE.*/
 // @name        AposLauncher
 // @namespace   AposLauncher
 // @include     http://agar.io/*
-// @version     4.267
+// @version     4.268
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposLauncherVersion = 4.267;
+var aposLauncherVersion = 4.268;
 
 var showAd = false;
 
