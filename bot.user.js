265f2851b7e1094062e7d50bd6234306473b2e81
Joe O'Donnell <joeod@contiigo.com>
2015-12-30 20:48:34 -0500

    perf

--- bot.user.js
+++ bot.user.js
@@ -33,11 +33,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1131
+// @version     3.1132
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1131;
+var aposBotVersion = 3.1132;
 
 var constants = {
 	splitRangeMin : 650,
@@ -1424,7 +1424,7 @@ function AposBot() {
 
 		if (approximate) {
 
-			return radiusSum * radiusSum + radiusSum + radiusSum > distanceX * distanceX + distanceY * distanceY; // 10%
+			return radiusSum * radiusSum + radiusSum * 3 > distanceX * distanceX + distanceY * distanceY; // 15%
 		}
 
 		return distanceX * distanceX + distanceY * distanceY <= radiusSum * radiusSum;
