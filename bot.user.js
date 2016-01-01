c156b3f04776e6f8dce12d6bd77377295c7074b0
Joe O'Donnell <joeod@contiigo.com>
2016-01-01 01:54:56 -0500

    threat weighting

--- bot.user.js
+++ bot.user.js
@@ -33,11 +33,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1185
+// @version     3.1186
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1185;
+var aposBotVersion = 3.1186;
 
 var constants = {
 	splitRangeMin : 650,
