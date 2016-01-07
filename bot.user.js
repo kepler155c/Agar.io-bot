63ed199ed30a256949a9e6e7f71ed55e9ef2f2fe
Joe O'Donnell <joeod@contiigo.com>
2016-01-06 20:14:30 -0500

    shoot viruses

--- bot.user.js
+++ bot.user.js
@@ -33,11 +33,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1321
+// @version     3.1322
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1321;
+var aposBotVersion = 3.1322;
 
 var constants = {
 	splitRangeMin : 650,
@@ -919,7 +919,7 @@ function AposBot() {
 
 			var virus = this.entities[key];
 
-			if (!closestVirus || virus.closestCell.distance < closestVirus.closestCell.distance) {
+			if (!closestVirus || virus.distance < closestVirus.distance) {
 				closestVirus = virus;
 			}
 
