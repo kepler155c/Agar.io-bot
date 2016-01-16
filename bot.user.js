dfbcec3631da35105c908a1f5994000e771d7cb3
Joe O'Donnell <joeod@contiigo.com>
2016-01-16 00:55:51 -0500

    random splitting

--- bot.user.js
+++ bot.user.js
@@ -34,11 +34,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1556
+// @version     3.1557
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1556;
+var aposBotVersion = 3.1557;
 
 var Constants = {
 	splitRangeMin : 650,
@@ -762,7 +762,7 @@ function AposBot() {
 
 					var entity = this.entities[key];
 
-					if (this.isRemoved) { // hack until the isRemoved is fixed
+					if (entity.isRemoved) { // hack until the isRemoved is fixed
 
 						entity.classification = Classification.unknown;
 
