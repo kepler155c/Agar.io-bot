0a2e2841bb968c50f3c131fe50b8e04a32fb16ce
Joe O'Donnell <joeod@contiigo.com>
2016-01-21 19:39:06 -0500

    perf

--- bot.user.js
+++ bot.user.js
@@ -34,11 +34,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1734
+// @version     3.1735
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1734;
+var aposBotVersion = 3.1735;
 
 var Constants = {
 
@@ -2185,7 +2185,9 @@ function AposBot() {
 	this.mainLoop = function(cells) {
 
 		if (this.noProcessing) {
-			return;
+			return {
+				override : false
+			};
 		}
 
 		if (!this.initialized) {
