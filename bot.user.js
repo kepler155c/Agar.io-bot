d63cc2d8bf0ea3826147c76833e3407a1dc2fd8a
Joe O'Donnell <joeod@contiigo.com>
2016-01-22 13:42:11 -0500

    always have a good angle

--- bot.user.js
+++ bot.user.js
@@ -34,11 +34,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1745
+// @version     3.1746
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1745;
+var aposBotVersion = 3.1746;
 
 var Constants = {
 
@@ -2184,7 +2184,7 @@ function AposBot() {
 			if (result.failed) {
 				console.log('last try');
 				console.log(result);
-				result = this.avoidThreats(player, destination, 4, result.max + 1);
+				result = this.avoidThreats(player, destination, 4, result.max);
 				console.log(result);
 			}
 		}
