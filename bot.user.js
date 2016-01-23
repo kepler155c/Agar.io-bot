4059e1524dd57aaa34daf7331a2c382676005f4a
Joe O'Donnell <joeod@contiigo.com>
2016-01-23 13:13:45 -0500

    angles

--- bot.user.js
+++ bot.user.js
@@ -34,11 +34,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1765
+// @version     3.1766
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1765;
+var aposBotVersion = 3.1766;
 
 var Constants = {
 
@@ -1503,7 +1503,10 @@ function AposBot() {
 				+ player.size, player.y + player.size, player.x, player.y));
 
 		drawPoint(player.x - player.size, player.y + player.size, Constants.yellow, Util.getAngle(player.x
-				+ player.size, player.y + player.size, player.x, player.y));
+				- player.size, player.y + player.size, player.x, player.y));
+
+		drawPoint(player.x - player.size, player.y, Constants.yellow, Util.getAngle(player.x, player.y + player.size,
+				player.x, player.y));
 
 		if (!doSplit && !shiftedAngle.shifted) {
 			player.lure(cluster, destination);
