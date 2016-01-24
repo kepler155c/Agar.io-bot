f7bc7072e959b0d1eadf6423ecb6226ad6343bb0
Joe O'Donnell <joeod@contiigo.com>
2016-01-24 00:22:17 -0500

    avoid threats

--- bot.user.js
+++ bot.user.js
@@ -34,11 +34,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1802
+// @version     3.1803
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1802;
+var aposBotVersion = 3.1803;
 
 var Constants = {
 
