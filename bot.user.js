9090657b11a5dd65276a6e6d3e1b9bb7e0a8f55e
Joe O'Donnell <joeod@contiigo.com>
2016-01-23 21:34:02 -0500

    avoid threats

--- bot.user.js
+++ bot.user.js
@@ -34,11 +34,11 @@ SOFTWARE.*/
 // @name        AposBot
 // @namespace   AposBot
 // @include     http://agar.io/*
-// @version     3.1791
+// @version     3.1792
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
 // ==/UserScript==
-var aposBotVersion = 3.1791;
+var aposBotVersion = 3.1792;
 
 var Constants = {
 
@@ -1873,7 +1873,7 @@ function AposBot() {
 
 	this.getRange = function(source, target) {
 
-		var radius = source.size;
+		var radius = source.size + target.size;
 
 		//Alpha
 		var a = Math.asin(radius / source.distance);
